a149536d03df1f1e723fb569fd874232fed78641
dogpoopy <qtqtron@gmail.com>
2024-01-07 23:24:02 +0800

    vayu: parts: Fix long press action of Dolby and Refreshrate QS tile
    * Redirect them to each services

--- parts/src/org/lineageos/settings/TileEntryActivity.java
+++ parts/src/org/lineageos/settings/TileEntryActivity.java
@@ -26,10 +26,14 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 import org.lineageos.settings.dirac.DiracActivity;
+import org.lineageos.settings.dolby.DolbyActivity;
+import org.lineageos.settings.refreshrate.RefreshActivity;
 
 public class TileEntryActivity extends Activity {
     private static final String TAG = "TileEntryActivity";
     private static final String DIRAC_TILE = "org.lineageos.settings.dirac.DiracTileService";
+    private static final String DOLBY_TILE = "org.lineageos.settings.dolby.DolbyTileService";
+    private static final String REFRESH_TILE = "org.lineageos.settings.refreshrate.RefreshTileService";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,6 +42,12 @@ public class TileEntryActivity extends Activity {
         switch (sourceClass.getClassName()) {
             case DIRAC_TILE:
                 openActivitySafely(new Intent(this, DiracActivity.class));
+                break;
+            case DOLBY_TILE:
+                openActivitySafely(new Intent(this, DolbyActivity.class));
+                break;
+            case REFRESH_TILE:
+                openActivitySafely(new Intent(this, RefreshActivity.class));
                 break;
             default:
                 finish();
